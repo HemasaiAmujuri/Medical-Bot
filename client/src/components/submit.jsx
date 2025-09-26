4256a035b5d64262e3ad3269f6e6763cfc7161d1
Amujuri Hema Sai <hemasaiamujuri3@gmail.com>
2025-09-26 03:06:52 -0700

    bug fixed

--- client/src/components/submit.jsx
+++ client/src/components/submit.jsx
@@ -5,10 +5,9 @@ import { useSelector } from "react-redux";
 
 function SubmitButton(){
 
-    // const {submittedBasicform, submittedLifestyleForm, submittedMedicalhistoryForm } = useSelector((state) => state.dataSlice );
      const submittedBasicform = useSelector((state) => state.basicForm.submittedBasicform);
-      const submittedLifestyleForm = useSelector((state) => state.basicForm.submittedLifestyleForm);
-       const submittedMedicalhistoryForm = useSelector((state) => state.basicForm.submittedMedicalhistoryForm);
+      const submittedLifestyleForm = useSelector((state) => state.lifestyleForm.submittedLifestyleForm);
+       const submittedMedicalhistoryForm = useSelector((state) => state.medicalHistoryForm.submittedMedicalhistoryForm);
 
     const latestFromsubmittedBasicform = submittedBasicform.length ? submittedBasicform[submittedBasicform.length - 1] : null;
   const latestFromsubmittedLifestyleForm = submittedLifestyleForm.length ? submittedLifestyleForm[submittedLifestyleForm.length - 1] : null;
