44da63a8ab5b031eb365c20be2b215d5282678f5
Amujuri Hema Sai <hemasaiamujuri3@gmail.com>
2025-09-28 23:37:21 -0700

    bug fixed

--- client/src/pages/signup/medicalHistory.jsx
+++ client/src/pages/signup/medicalHistory.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { medicalHistoryForm } from "../../actions/actions";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 function Medicaldetails() {
@@ -13,13 +13,17 @@ function Medicaldetails() {
 
 
   const dispatch = useDispatch();
+
+
+  useEffect(() => {
   dispatch(medicalHistoryForm({
-    chronic : chronic,
-    medications : medications,
-    allergies: allergies,
-    surgeries: surgeries,
-    health : health
+    chronic,
+    medications,
+    allergies,
+    surgeries,
+    health
   }));
+}, [chronic, medications, allergies, surgeries, health, dispatch]);
 
 
    
@@ -29,10 +33,11 @@ function Medicaldetails() {
 
         <div className="flex flex-col w-100">
           <h1 className="text-5xl font-bold mb-4 ml-5 mt-10 bg-black- w-150"> MEDICAL HISTORY </h1>
-          <label className="text-2xl w-150 font-bold pl-5"> Do you have any chronic illnesses? </label>
+          <label htmlFor="chronic" className="text-2xl w-150 font-bold pl-5"> Do you have any chronic illnesses? </label>
           <textarea
             rows="3"
-            cols="30"         
+            cols="30"
+            id = "chronic"        
             required
             onChange={(e) => setChronic(e.target.value)}
             className="ml-5 pl-4  bg-gray-300 "
@@ -40,10 +45,11 @@ function Medicaldetails() {
         </div>
 
          <div className="flex flex-col w-100">
-          <label className="text-2xl w-150 font-bold pl-5"> Are you currently taking any medications? </label>
+          <label htmlFor="medications" className="text-2xl w-150 font-bold pl-5"> Are you currently taking any medications? </label>
           <textarea
             rows="3"
-            cols="30"         
+            cols="30"
+            id="medications"         
             required
              onChange={(e) => setMedications(e.target.value)}
             className="ml-5 pl-4  bg-gray-300 "
@@ -51,10 +57,11 @@ function Medicaldetails() {
         </div>
 
         <div className="flex flex-col w-100">
-          <label className="text-2xl w-150 font-bold pl-5"> Do you have any allergies?</label>
+          <label htmlFor = "allergies" className="text-2xl w-150 font-bold pl-5"> Do you have any allergies?</label>
           <textarea
             rows="3"
-            cols="30"         
+            cols="30"
+            id="allergies"       
             required
             onChange={(e) => setAllergies(e.target.value)}
             className="ml-5 pl-4  bg-gray-300 "
@@ -63,10 +70,11 @@ function Medicaldetails() {
 
 
         <div className="flex flex-col w-100">
-          <label className="text-2xl w-150 font-bold pl-5"> Have you had any surgeries in the past? </label>
+          <label htmlFor = "surgeries" className="text-2xl w-150 font-bold pl-5"> Have you had any surgeries in the past? </label>
           <textarea
             rows="3"
-            cols="30"         
+            cols="30"
+            id = "surgeries"         
             required
              onChange={(e) => setSurgeries(e.target.value)}
             className="ml-5 pl-4  bg-gray-300 "
@@ -75,10 +83,11 @@ function Medicaldetails() {
 
 
            <div className="flex flex-col w-100">
-          <label className="text-2xl w-150 font-bold pl-5"> Any recent symptoms or health concerns? </label>
+          <label htmlFor="health" className="text-2xl w-150 font-bold pl-5"> Any recent symptoms or health concerns? </label>
           <textarea
             rows="3"
-            cols="30"         
+            cols="30"
+            id ="health"         
             required
              onChange={(e) => setHealth(e.target.value)}
             className="ml-5 pl-4  bg-gray-300 "
